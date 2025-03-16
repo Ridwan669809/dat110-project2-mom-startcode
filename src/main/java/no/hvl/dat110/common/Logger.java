18f49768f2ac42077a1190793afff332c1fe30f1
Ridwan669809 <669809@stud.hvl.no>
2025-03-16 22:58:29 +0100

    Initial commit

--- src/main/java/no/hvl/dat110/common/Logger.java
+++ src/main/java/no/hvl/dat110/common/Logger.java
@@ -2,19 +2,12 @@ package no.hvl.dat110.common;
 
 public class Logger {
 
-	public static boolean debug = true;
+    public static boolean debug = true;
 
-	public static void log(String s) {
+    public static void log(String s) {
 
-		if (debug) {
-			System.out.println(s);
-		}
-	}
-	
-	public static void lg(String s) {
-
-		if (debug) {
-			System.out.print(s);
-		}
-	}
+        if (debug) {
+            System.out.println(s);
+        }
+    }
 }
